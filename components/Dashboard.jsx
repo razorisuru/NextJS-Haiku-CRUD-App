38455c11a62ed4a0abb0a5720c006b5828ef3623
Isuru Bandara <isurubandara318@gmail.com>
2025-06-01 00:13:32 +0530

    udate

--- components/Dashboard.jsx
+++ components/Dashboard.jsx
@@ -1,8 +1,6 @@
 import { getCollection } from "@/lib/db";
 import { ObjectId } from "mongodb";
-import Link from "next/link";
-import { cookies } from "next/headers";
-import { deleteHaiku } from "@/actions/haikuController";
+import Haiku from "./Haiku";
 
 async function getHaikus(id) {
   const collection = await getCollection("haikus");
@@ -15,39 +13,15 @@ async function getHaikus(id) {
 }
 
 export default async function Dashboard(props) {
-  const cookieStore = cookies();
-  const success = cookieStore.get("success")?.value;
   const haikus = await getHaikus(props.user.userId);
+
   return (
-    <div className="container mx-auto p-10">
-
-      <h2 className="text-center text-2xl text-gray-500 mb-5">Your Haikus</h2>
-
-
+    <div>
+      <h2 className="text-center text-2xl text-gray-600 mb-5">Your Haikus</h2>
       {haikus.map((haiku, index) => {
-        return (
-          <div
-            key={index}
-            className="bg-blue-400 p-5 rounded-lg shadow-md mb-5"
-          >
-            <h3 className="text-xl font-semibold">{haiku.line1}</h3>
-            <p className="text-gray-700 mt-2">{haiku.line2}</p>
-            <p className="text-gray-700 mt-2">{haiku.line3}</p>
-            <p className="text-gray-500 mt-2 text-sm">
-              Created at: {new Date(haiku.createdAt).toLocaleDateString()}
-            </p>
-            <Link href={`/edit-haiku/${haiku._id.toString()}`}>EDIT</Link>
-            <form action={deleteHaiku}>
-              <input type="hidden" name="id" value={haiku._id.toString()} />
-              <button
-                type="submit"
-                className="text-red-600 cursor-auto"
-              >
-                DELETE
-              </button>
-            </form>
-          </div>
-        );
+        haiku._id = haiku._id.toString();
+        haiku.author = haiku.author.toString();
+        return <Haiku haiku={haiku} key={index} />;
       })}
     </div>
   );
